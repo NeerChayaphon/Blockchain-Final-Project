200d2c400b4ffd3651a253df347266bda1ec137b
chayaphon bunyakan <nchayaphon.bun@gmail.com>
2022-05-12 19:29:34 +0700

    Navbar Dashboard

--- src/App.js
+++ src/App.js
@@ -1,100 +1,117 @@
 import React, { useEffect, useState } from 'react';
 import Renderer from './Renderer.js';
-import Certification from "./Certification.json"
+import Certification from './Certification.json';
 import Certificates from './Certificates.js';
 import Navbar from './Navbar.js';
+import { Box } from '@chakra-ui/react';
 
 // https://github.com/mnzsss/react-chakra-pagination
 
-const ipfsAPI = require('ipfs-api')
-const ipfs = ipfsAPI('ipfs.infura.io', '5001', { protocol: 'https' })
+const ipfsAPI = require('ipfs-api');
+const ipfs = ipfsAPI('ipfs.infura.io', '5001', { protocol: 'https' });
 const ethers = require('ethers');
 
 function App() {
-
   const [contract, setContract] = useState();
   const [buffer, setBuffer] = useState();
-  const [certificates, setCertificates] = useState([])
+  const [certificates, setCertificates] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  
   const connectBlockChain = async () => {
-    setLoading(true)
+    setLoading(true);
     await window.ethereum.enable();
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
     const signer = provider.getSigner();
-    const contract_address = process.env.REACT_APP_CONTRACT_ADDRESS //local
-    let deployedContract = new ethers.Contract(contract_address, Certification.abi, signer)
+    const contract_address = process.env.REACT_APP_CONTRACT_ADDRESS; //local
+    let deployedContract = new ethers.Contract(
+      contract_address,
+      Certification.abi,
+      signer
+    );
     setContract(deployedContract);
 
     await fetchImgages(deployedContract);
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
   // Load certificates
-  const fetchImgages = async (contract) => {
-    const certificatesCount = await contract.certificateCount()
+  const fetchImgages = async contract => {
+    const certificatesCount = await contract.certificateCount();
     for (var i = 1; i <= certificatesCount.toNumber(); i++) {
       const certificate = await contract.certificates(i);
       certificates.push(certificate);
-
     }
-  }
+  };
 
   useEffect(() => {
     const load = async () => {
       await connectBlockChain();
-     
-    }
+    };
     load();
   }, []);
 
   const captureFile = event => {
-    event.preventDefault()
-    const file = event.target.files[0]
-    const reader = new window.FileReader()
-    reader.readAsArrayBuffer(file)
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
     reader.onloadend = () => {
-      let buff = Buffer(reader.result)
+      let buff = Buffer(reader.result);
       setBuffer(buff);
-    }
-  }
+    };
+  };
 
-  const uploadCertificate = async (description) => {
+  const uploadCertificate = async (description,owner) => {
     //adding file to the IPFS
     ipfs.files.add(buffer, async (error, result) => {
-      console.log('Ipfs result = ', result)
+      console.log('Ipfs result = ', result);
       if (error) {
-        console.error(error)
-        return
+        console.error(error);
+        return;
       }
       //uploading hash to blockchain
-      const testOwner = "Neer"
-      const tx = await contract.uploadCertificate(result[0].hash,testOwner,description);
-      console.log("tx = ", tx)
+      const testOwner = 'Neer';
+      const tx = await contract.uploadCertificate(
+        result[0].hash,
+        owner,
+        description
+      );
+      console.log('tx = ', tx);
       const receipt = await tx.wait();
-      console.log("receipt = ", receipt)
+      console.log('receipt = ', receipt);
 
       window.location.reload(false);
-    })
-  }
-
+    });
+  };
 
   return (
-    <div className="App">
-      <Navbar/>
-      { loading
-        ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-        : <Certificates/>
-        // <Renderer
-        //   certificates={certificates}
-        //   captureFile={captureFile}
-        //   uploadCertificate={uploadCertificate}
-        // />
+    <Box h="calc(100vh)" bg={'gray.50'}>
+      <Navbar />
+
+      {
+        loading ? (
+          <div id="loader" className="text-center mt-5">
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <Renderer
+          certificates={certificates}
+          captureFile={captureFile}
+          uploadCertificate={uploadCertificate}
+        />
+        )
+        
+        
+        // (
+        //   <Certificates
+        //     uploadCertificate={uploadCertificate}
+        //     captureFile={captureFile}
+        //   />
+        // )
+        
       }
-    </div>
+    </Box>
   );
 }
 export default App;
-
--- src/Renderer.js
+++ src/Renderer.js
@@ -1,79 +1,110 @@
 import React from 'react';
 import Identicon from 'identicon.js';
+import Certificates from './Certificates'
+
+import {
+  Container,
+  Heading,
+  Text,
+  Button,
+  HStack,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Box,
+} from '@chakra-ui/react';
 const ethers = require('ethers');
+
 const Renderer = ({ certificates, uploadCertificate, captureFile }) => {
-  // certificate[0] = new Date(certificate[0].toNumber() * 1000).toLocaleString();
-  let certificateDescription = '';
+  // certificate[0] = new Date(certificate[0].toNumber() * 1000).toLocaleString()
   return (
-    <div className="container-fluid mt-5">
-      <div className="row">
-        <div className="content mr-auto ml-auto">
-          <h2>Share Certificate</h2>
-          <form
-            onSubmit={event => {
-              event.preventDefault();
-              const description = certificateDescription.value;
-              uploadCertificate(description, captureFile);
-            }}
-          >
-            <input
-              type="file"
-              accept=".jpg, .jpeg, .png, .bmp, .gif"
-              onChange={captureFile}
-            />
-            <div className="form-group mr-sm-2">
-              <br></br>
-              <input
-                id="certificateDescription"
-                type="text"
-                ref={input => {
-                  certificateDescription = input;
-                }}
-                className="form-control"
-                placeholder="Certificate description..."
-                required
-              />
-            </div>
-            <button type="submit" className="btn btn-primary btn-block btn-lg">
-              Upload!
-            </button>
-          </form>
-          <p>&nbsp;</p>
-          {certificates.map((certificate, key) => {
-            return (
-              <div className="card mb-4" key={key}>
-                <div className="card-header">
-                  <img
-                    className="mr-2"
-                    width="30"
-                    height="30"
-                    src={`data:certificate/png;base64,${new Identicon(
-                      certificate.author,
-                      30
-                    ).toString()}`}
-                  />
-                  <small className="text-muted">{certificate.author}</small>
+    <div>
+      {/* {certificates.map((certificate, key) => {
+              return (
+                <div className="card mb-4" key={key}>
+                  <div className="card-header">
+                    <img
+                      className="mr-2"
+                      width="30"
+                      height="30"
+                      src={`data:certificate/png;base64,${new Identicon(
+                        certificate.author,
+                        30
+                      ).toString()}`}
+                    />
+                    <small className="text-muted">{certificate.author}</small>
+                  </div>
+                  <ul
+                    id="certificateList"
+                    className="list-group list-group-flush"
+                  >
+                    <li className="list-group-item">
+                      <p className="text-center">
+                        <img
+                          src={`https://ipfs.infura.io/ipfs/${certificate.hash}`}
+                          style={{ maxWidth: '420px' }}
+                        />
+                      </p>
+                      <p>{certificate.description}</p>
+                      <p>{new Date(certificate.timestamp.toNumber() * 1000).toLocaleString()}</p>
+                    </li>
+                  </ul>
                 </div>
-                <ul
-                  id="certificateList"
-                  className="list-group list-group-flush"
-                >
-                  <li className="list-group-item">
-                    <p className="text-center">
-                      <img
-                        src={`https://ipfs.infura.io/ipfs/${certificate.hash}`}
-                        style={{ maxWidth: '420px' }}
-                      />
-                    </p>
-                    <p>{certificate.description}</p>
-                    <p>{new Date(certificate.timestamp.toNumber() * 1000).toLocaleString()}</p>
-                  </li>
-                </ul>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+              );
+            })} */}
+
+      <Container maxW={'container.lg'} p={0}>
+        <HStack justifyContent={'space-between'} marginTop={10} paddingY={5}>
+          <Heading>Certificates</Heading>
+          <Certificates
+            uploadCertificate={uploadCertificate}
+            captureFile={captureFile}
+          />
+        </HStack>
+
+        <TableContainer bg={'white'}>
+          <Table variant="simple">
+            <TableCaption>Imperial to metric conversion factors</TableCaption>
+            <Thead>
+              <Tr>
+                <Th w={2}>Owner</Th>
+                <Th w={2}>Published Date</Th>
+                <Th w={2} s>
+                  Details
+                </Th>
+                <Th isNumeric></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {certificates.map((certificate, key) => {
+                return (
+                  <Tr>
+                    <Td>{certificate.owner}</Td>
+                    <Td>
+                      {new Date(
+                        certificate.timestamp.toNumber() * 1000
+                      ).toLocaleString()}
+                    </Td>
+                    <Td>
+                      <Text w={'sm'}>{certificate.description}</Text>
+                    </Td>
+                    <Td>
+                      <Button mr={2}>View</Button>
+                      <Button>Share</Button>
+                    </Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Container>
     </div>
   );
 };
